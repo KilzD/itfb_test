d750fa2910dfc6c606f7684d1852abc01c8c5cb6
Vlad Drozdov <vyudrozdov@edu.hse.ru>
2019-09-04 22:16:30 +0300

    Updates

---
+++ precache-manifest.e87528ecd2de4f58cc9b67030405bdf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd791182a91292605a090b8429e85c5",
+    "url": "/itfb_test/index.html"
+  },
+  {
+    "revision": "10a1fd2a2b95be660895",
+    "url": "/itfb_test/static/css/main.09718e25.chunk.css"
+  },
+  {
+    "revision": "7b203f13968c84601df1",
+    "url": "/itfb_test/static/js/2.56142cfd.chunk.js"
+  },
+  {
+    "revision": "10a1fd2a2b95be660895",
+    "url": "/itfb_test/static/js/main.dbafcc59.chunk.js"
+  },
+  {
+    "revision": "c96026add23e8b72655b",
+    "url": "/itfb_test/static/js/runtime~main.162260a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/itfb_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
